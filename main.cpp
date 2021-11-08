23ac117a7e17dc71c662b0775d1d5aab3895c6da
piwo <you@example.com>
2021-11-08 12:32:20 +0100

    add std greater comparator.

--- main.cpp
+++ main.cpp
@@ -5,21 +5,23 @@
 
 int main()
 {
-  int idx=0;
+  int idx = 0;
   std::string ss{"*Hello World!*"};
-  std::map<std::string, int> mapOfWords;
-  std::map<char,int> mapChar;
-  
-  std::cout << "Size of " << ss << ss.size() <<std::endl;
-  for(auto s = ss.rbegin(); s < ss.rend(); s++)
-	  std::cout << *s;
-  for(auto &s : ss)
-     { 
-       //mapChar.insert(std::make_pair(s,idx++)); 
-       ++mapChar[s];
-     }
+  std::map<std::string, int, std::greater<std::string>> mapOfWords;
+  std::map<char, int,std::greater<char>> mapChar;
 
-	for(auto it=mapChar.begin(); it!=mapChar.end();it++)
-       std::cout << it->first << "  " << it->second << std::endl;   
-return 0;
+  std::cout << "Size of " << ss << ss.size() << std::endl;
+  for (auto s = ss.rbegin(); s < ss.rend(); s++)
+    std::cout << *s;
+  for (auto &s : ss)
+  {
+    //mapChar.insert(std::make_pair(s,idx++));
+    ++mapChar[s];
+  }
+
+  for (auto it = mapChar.begin(); it != mapChar.end(); it++)
+    std::cout << it->first << "  " << it->second << std::endl;
+
+
+  return 0;
 }
