47c04515c37c852883a5baceb20db1ab28e5ce92
piwo <you@example.com>
2021-11-08 11:36:32 +0100

    add size of.

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@
 int main()
 {
   std::string ss{"*Hello World!*"};
-  std::cout << ss << std::endl;
+  std::cout << "Size of " << ss << ss.size() <<std::endl;
   for(auto s = ss.rbegin(); s < ss.rend(); s++)
 	  std::cout << *s;
  
