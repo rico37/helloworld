399f015cbc1897d5754a2ec8b590fb1dbceb7a45
piwo <you@example.com>
2021-11-08 12:07:07 +0100

    add map.

--- main.cpp
+++ main.cpp
@@ -1,12 +1,21 @@
 #include <iostream>
 #include <string>
+#include <map>
+#include <iterator>
 
 int main()
 {
+  int idx=0;
   std::string ss{"*Hello World!*"};
+  std::map<std::string, int> mapOfWords;
+  std::map<char,int> mapChar;
+  
   std::cout << "Size of " << ss << ss.size() <<std::endl;
   for(auto s = ss.rbegin(); s < ss.rend(); s++)
 	  std::cout << *s;
- 
+  for(auto &s : ss)
+     mapChar.insert(std::make_pair(s,idx++));
+	for(auto it=mapChar.begin(); it!=mapChar.end();it++)
+       std::cout << it->first << "  " << it->second << std::endl;   
 return 0;
 }
